46e8a03de87e68deffd8fc51a56aefe2895c3c8a
sshera-dlaw <saritilawah66@gmail.com>
2024-06-19 14:07:17 +0900

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+print(string.ascii_letters)
+print(string.digits)
+
+data = int(input("Panjang Password yang diinginkan: "))
+generator = string.ascii_letters + string.digits
+x = []
+for i in range(data):
+    x.append(random.choice(generator))
+print(x)
